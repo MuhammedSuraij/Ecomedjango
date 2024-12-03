3cae0144d82d9a2c7a6d1d4366e6d8a9e711b519
MuhammedSuraij <msuraijcv@gmail.com>
2024-12-03 11:37:33 +0530

    models created and some layout designed

---
+++ ecome/products/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('',views.indexview,name='home'),
+    path('products_list',views.list_products,name='list_product'),
+
+   
+
+]
+
